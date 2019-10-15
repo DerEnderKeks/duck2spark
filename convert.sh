a1af11ebf333f5ad82ab9164f9c4553a433b328f
DerEnderKeks <DerEnderKeks@users.noreply.github.com>
2019-10-15 13:44:53 +0200

    Update and rename example.sh to convert.sh

---
+++ convert.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: $0 [<input> [output]]"
+  echo -e "Use \e[34m\$DUCKLANG\e[0m to set the keyboard language (default: \e[34mde\e[0m)."
+  exit 0
+fi
+
+if [ -z "$DUCKLANG" ]; then
+  DUCKLANG="de"
+fi
+
+
+if [ -z "$1" ]; then
+  INFILE="spark.duck"
+else
+  INFILE="$1"
+fi
+
+if [ -z "$2" ]; then
+  OUTFILE="spark.ino"
+else
+  OUTFILE="$2"
+fi
+
+echo -e "Converting \e[34m$INFILE\e[0m"
+echo -e "        to \e[34m$OUTFILE\e[0m\n"
+
+TMPBINFILE=$(mktemp $TEMP/spark.bin.XXXXXX)
+
+check_error () {
+  if [ "$2" != "0" ]; then
+    echo "$1 failed! (exit code $2)"
+	exit "$2"
+  fi
+}
+
+java -jar encoder.jar -i "$INFILE" -o "$TMPBINFILE" -l "$DUCKLANG"
+check_error "Encoder" $?
+
+python duck2spark.py -i "$TMPBINFILE" -f 0 -o "$OUTFILE"
+check_error "duck2spark" $?
+
+rm "$TMPBINFILE" 
+
+echo "Done."
+exit 0
